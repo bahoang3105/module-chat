37e0fb0325a57c4ee42e870414fb9dd12b577a5c
bahoang3105 <73835461+bahoang3105@users.noreply.github.com>
2021-12-30 17:53:38 +0700

    ES6

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ let typeListFriend = 'online';
 let listFriendOnline;
 let listFriendHistory;
 
-function renderHeaderChat(avatar, name, state) {
+const renderHeaderChat = (avatar, name, state) => {
   let header = `
     <img class='avatar-chat' src='${avatar}' alt='user-avatar'>
     <div>
@@ -13,7 +13,7 @@ function renderHeaderChat(avatar, name, state) {
   document.getElementById('header-chat').innerHTML = header;
 }
 
-function changeList(type) {
+const changeList = async (type) => {
   if(typeListFriend === type) return;
   typeListFriend = type;
   if(type === 'online') {
@@ -23,29 +23,40 @@ function changeList(type) {
     document.getElementById('chat-field-title-history').style.backgroundColor = 'rgb(238, 238, 238)';
     document.getElementById('chat-field-title-online').style.backgroundColor = 'rgb(211, 211, 211)';
   }
-  renderListFriend(type);
+  await renderListFriend(type);
 }
 
-function renderListFriend(type) {
+const renderListFriend = async (type) => {
   if(type === 'online') {
-    renderListFriendOnline();
-  } else {
-    renderListFriendHistory();
-  }
+    return await renderListFriendOnline();
+  } 
+  return await renderListFriendHistory();
 }
 
-async function renderListFriendOnline() {
+const renderListFriendOnline = async () => {
   if(listFriendOnline === undefined) {
     // request API get list friend online
     // fake data
-    listFriendOnline = [];
+    listFriendOnline = [
+      { name: 'Pleiku', avatar: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eso.org%2Fpublic%2Fimages%2F&psig=AOvVaw24ES4b1aThwyZ5BmTgBKrD&ust=1640947340141000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIDFw66ri_UCFQAAAAAdAAAAABAE' },
+      { name: 'Hoang Anh', avatar: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eso.org%2Fpublic%2Fimages%2F&psig=AOvVaw24ES4b1aThwyZ5BmTgBKrD&ust=1640947340141000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIDFw66ri_UCFQAAAAAdAAAAABAE' },
+      { name: 'Gia Lai', avatar: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eso.org%2Fpublic%2Fimages%2F&psig=AOvVaw24ES4b1aThwyZ5BmTgBKrD&ust=1640947340141000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIDFw66ri_UCFQAAAAAdAAAAABAE' },
+      { name: 'Suduku', avatar: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eso.org%2Fpublic%2Fimages%2F&psig=AOvVaw24ES4b1aThwyZ5BmTgBKrD&ust=1640947340141000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIDFw66ri_UCFQAAAAAdAAAAABAE' },
+      { name: 'Bokukiko', avatar: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eso.org%2Fpublic%2Fimages%2F&psig=AOvVaw24ES4b1aThwyZ5BmTgBKrD&ust=1640947340141000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIDFw66ri_UCFQAAAAAdAAAAABAE' },
+      { name: 'Meo Meo', avatar: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eso.org%2Fpublic%2Fimages%2F&psig=AOvVaw24ES4b1aThwyZ5BmTgBKrD&ust=1640947340141000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIDFw66ri_UCFQAAAAAdAAAAABAE' },
+      { name: 'Yen', avatar: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eso.org%2Fpublic%2Fimages%2F&psig=AOvVaw24ES4b1aThwyZ5BmTgBKrD&ust=1640947340141000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIDFw66ri_UCFQAAAAAdAAAAABAE' },
+    ];
   }
-  for(let i = 0; i < listFriendOnline.length; i++) {
-
-  }
+  let listFriend = `<div>${listFriendOnline.map(friend => renderFriendOnline(friend.name, friend.avatar))}</div>`;
+  // let listFriend = '<div>';
+  // for(let i = 0; i < listFriendOnline.length; i++) {
+  //   listFriend = listFriend + renderFriendOnline(listFriendOnline[i].name, listFriendOnline[i].avatar);
+  // }
+  // listFriend = listFriend + '</div>';
+  document.getElementById('list-friend-chat').innerHTML = listFriend;
 }
 
-async function renderListFriendHistory() {
+const renderListFriendHistory = async () => {
   if(listFriendHistory === undefined) {
     // request API get chat history
     // fake data
@@ -53,11 +64,11 @@ async function renderListFriendHistory() {
   }
 }
 
-function renderFriendOnline(name, avatar) {
-
+const renderFriendOnline = (name, avatar) => {
+  return `<div>${name}</div>`;
 }
 
-function renderFriendHistory(name, avatar, latestMessage, isRead) {
-  
+const renderFriendHistory = (name, avatar, latestMessage, isRead) => {
+
 }
 
